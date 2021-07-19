7abbad8b33346348325973b3dfd228e45b66ce22
unknown <tula3and@gmail.com>
2021-07-20 05:55:05 +0900

    Add monthly plans

---
+++ js/login.js
@@ -0,0 +1,27 @@
+const nameInput = document.querySelector("#login");
+const username = document.querySelector("#input-name");
+const welcome = document.querySelector("#user");
+
+const USER_KEY = "user";
+const HIDDEN_CLASS = "hidden";
+
+function handleNameSubmit(event) {
+    event.preventDefault();
+    localStorage.setItem(USER_KEY, username.value);
+    typingUsername(username.value);
+    nameInput.classList.add(HIDDEN_CLASS);
+}
+
+function typingUsername(n) {
+    welcome.innerText = `안녕하세요 ${n} 님!`;
+    welcome.classList.remove(HIDDEN_CLASS);
+}
+
+if (localStorage.getItem(USER_KEY) !== null) {
+    typingUsername(localStorage.getItem(USER_KEY));
+    welcome.classList.remove(HIDDEN_CLASS);
+}
+else {
+    nameInput.addEventListener("submit", handleNameSubmit);
+    nameInput.classList.remove(HIDDEN_CLASS);
+}
