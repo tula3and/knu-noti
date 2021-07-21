f9d303bf9ebddd24ad80d5c94c0c39edc7df3120
unknown <seungxxyun@gmail.com>
2021-07-21 18:35:31 +0900

    update

--- js/todo.js
+++ js/todo.js
@@ -1,24 +1,43 @@
 const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
+const completeList = document.getElementById("complete-list");
 const TODOS_KEY = "todos";
+const COMPLETE_KEY = "complete";
 
 let toDos = [];
+let completeToDos = [];
 
 function saveToDos() {
   localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
 }
 
-/*function completeToDo(event) {
+function saveComplete() {
+  localStorage.setItem(COMPLETE_KEY, JSON.stringify(completeToDos));
+}
+
+function completeToDo(event) {
   const li = event.target.parentElement;
-  li.classList.toggle("checked");
-  if (li.classList.value === "checked") {
-    li.style.textDecoration = "line-through";
+  const complete = document.createElement("li");
+  complete.id = li.id;
+  const span = document.createElement("span");
+  let i = 0;
+  for (let j = 0; j < toDos.length; j++) {
+    if (parseInt(toDos[j]["id"]) === parseInt(li.id)) {
+      i = j; 
+    }
   }
-  else {
-    li.style.textDecoration = "none";
-  }
-}*/
+  span.innerText = toDos[i]["text"];
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "❌";
+  deleteButton.addEventListener("click", deleteToDo);
+  complete.appendChild(span);
+  complete.appendChild(deleteButton);
+  completeList.appendChild(complete);
+  deleteToDo(event);
+  completeToDos.push({"text": span.innerText, id: li.id});
+  saveComplete();
+}
 
 function deleteToDo(event) {
   const li = event.target.parentElement;
@@ -27,19 +46,41 @@ function deleteToDo(event) {
   saveToDos();
 }
 
+function deleteComplete(event) {
+  const li = event.target.parentElement;
+  li.remove();
+  completeToDos = completeToDos.filter((complete) => parseInt(complete.id) !== parseInt(li.id));
+  saveComplete();
+}
+
+const newComplete = {"text": "", "id": 0};
+
+function paintComplete(newComplete) {
+  const li = document.createElement("li");
+  li.id = newComplete.id;
+  const span = document.createElement("span");
+  span.innerText = newComplete["text"];
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "❌";
+  deleteButton.addEventListener("click", deleteComplete);
+  li.appendChild(span);
+  li.appendChild(deleteButton);
+  completeList.appendChild(li);
+}
+
 function paintToDo(newTodo) {
   const li = document.createElement("li");
   li.id = newTodo.id;
   const span = document.createElement("span");
   span.innerText = newTodo.text;
-  //const completeButton = document.createElement("button");
-  //completeButton.innerText = "✔️";
-  //completeButton.addEventListener("click", completeToDo);
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "✔️";
+  completeButton.addEventListener("click", completeToDo);
   const deleteButton = document.createElement("button");
   deleteButton.innerText = "❌";
   deleteButton.addEventListener("click", deleteToDo);
   li.appendChild(span);
-  //li.appendChild(completeButton);
+  li.appendChild(completeButton);
   li.appendChild(deleteButton);
   toDoList.appendChild(li);
 }
@@ -60,9 +101,16 @@ function handleToDoSubmit(event) {
 toDoForm.addEventListener("submit", handleToDoSubmit);
 
 const savedToDos = localStorage.getItem(TODOS_KEY);
+const savedComplete = localStorage.getItem(COMPLETE_KEY);
 
 if (savedToDos !== null) {
   const parsedToDos = JSON.parse(savedToDos);
   toDos = parsedToDos;
   parsedToDos.forEach(paintToDo);
+}
+
+if (savedComplete !== null) {
+  const parsedComplete = JSON.parse(savedComplete);
+  completeToDos = parsedComplete;
+  parsedComplete.forEach(paintComplete);
 }
