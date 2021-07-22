796f0e821ef37af55cc9c6d2c379fc8d133f6503
good797979 <good79797979@naver.com>
2021-07-23 02:56:36 +0900

    fix : prompt answer

--- js/linkUpdate.js
+++ js/linkUpdate.js
@@ -16,18 +16,21 @@ function saveLink() {
 }
 
 function addLink() {
-    const newLink = prompt("추가하고 싶은 링크를 넣어주세요!", "http://www.knu.ac.kr/wbbs/");
-    const newLinkName = prompt("링크 이름은 뭘로 정할까요?", "경북대학교");
-    if (newLink === null || newLinkName === null) return;
-    const buttonLink = document.createElement("a");
-    const newButton = document.createElement("button");
-    buttonLink.href = newLink;
-    newButton.innerText = newLinkName;
-    buttonLink.appendChild(newButton);
-    floatingIcon.appendChild(buttonLink);
-    linkList.push({"linkName": newLinkName, "link": newLink});
-    saveLink();
-    linkLenCheck();
+    const newLink = prompt("추가하고 싶은 링크를 넣어주세요!", "http://www.knu.ac.kr/wbbs/");    
+    if(newLink!=null){
+        newLinkName = prompt("링크 이름은 뭘로 정할까요?", "경북대학교");
+        if(newLinkName!=null){
+            const buttonLink = document.createElement("a");
+            const newButton = document.createElement("button");
+            buttonLink.href = newLink;
+            newButton.innerText = newLinkName;
+            buttonLink.appendChild(newButton);
+            floatingIcon.appendChild(buttonLink);
+            linkList.push({"linkName": newLinkName, "link": newLink});
+            saveLink();
+            linkLenCheck();
+        }else return;
+    }else return;
 }
 
 function paintList(listObj) {
